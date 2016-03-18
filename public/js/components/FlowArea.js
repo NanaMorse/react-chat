8b4d3ffa3ff4c889d9a9ea6b183ee9f145f70686
Morse_Guo <morse@xmind.net>
2016-03-18 16:29:58 +0800

    初步完成模块间消息通讯

--- public/js/components/FlowArea.js
+++ public/js/components/FlowArea.js
@@ -2,13 +2,41 @@
  * Created by nana on 16/3/18.
  */
 var React  = require('react');
+var eventEmitter = require('wolfy87-eventemitter');
+
+var MsgList = React.createClass({
+  render : function(){
+    var createList = function(item){
+      return <p key={item.id} className={item.className}>{item.msg}</p>;
+    };
+
+    return <div>{this.props.flows.map(createList)}</div>;
+  }
+});
+
 
 module.exports = React.createClass({
+  getInitialState : function(){
+    return { flows : [] };
+  },
+
+  componentDidMount : function(){
+    eventEmitter.on('sendMsg', this.onNewMsg);
+  },
+
+  onNewMsg : function(msg, className){
+    var nextFlows = this.state.flows.concat({
+      msg : msg,
+      className : className,
+      id : Date.now()
+    });
+    this.setState({ flows : nextFlows });
+  },
+
   render : function(){
     return (
       <div className="flowArea">
-        <p className="hasSend">Hello!</p>
-        <p className="hasReceive">How Are You!</p>
+        <MsgList flows={this.state.flows}/>
       </div>
     );
   }
--- public/js/components/NewMsgArea.js
+++ public/js/components/NewMsgArea.js
@@ -2,14 +2,36 @@
  * Created by nana on 16/3/18.
  */
 var React  = require('react');
+var eventEmitter = require('wolfy87-eventemitter');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return { newMsg : '' };
+  },
+
   render : function(){
     return (
       <div className="newMsgArea">
-        <input type="text" className="editIpt"/>
-        <button className="sendBtn">Send</button>
+        <form onSubmit={this.sendMsg}>
+          <input type="text" className="editIpt" onChange={this.onChange} value={this.state.newMsg}/>
+          <button className="sendBtn">送出</button>
+        </form>
       </div>
     );
+  },
+
+  onChange : function(e){
+    this.setState({ newMsg : e.target.value });
+  },
+
+  sendMsg : function(e){
+    e.preventDefault();
+    var newMsg = this.state.newMsg;
+    if(!newMsg) return;
+
+    eventEmitter.trigger('sendMsg', [this.state.newMsg, 'hasSend']);
+
+    newMsg = '';
+    this.setState({ newMsg : newMsg });
   }
 });
